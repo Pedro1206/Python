c6554e9778ae2eb6ed016fc3e080162d75b89670
Pedro1206 <73972095+Pedro1206@users.noreply.github.com>
2020-11-04 22:49:21 -0300

    Create Programa que pede 2 números e retorna as 4 operações principais.py

---
+++ Programa que pede 2 números e retorna as 4 operações principais.py
@@ -0,0 +1,8 @@
+print("Informe dois nímeros inteiros")
+n1 = int(input("Digite o primeiro número"))
+n2 = int(input("Digite o segundo número:"))
+
+print("Soma = ", n1+n2)
+print("Multiplicação = ", n1*n2)
+print("Diferença = ", n1-n2)
+print("Divisão = ", n1/n2)
