9edbb58344f8ad6e7de5548ffb7f3c8160ee4c52
Pedro Santos <73972095+Pedro1206@users.noreply.github.com>
2020-11-23 13:15:45 -0300

    Create Calculadora por função.py

---
+++ Calculadora por função.py
@@ -0,0 +1,22 @@
+a = int(input("1 valor:"))
+b = int(input("2 valor:"))
+
+class Calculadora:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    def soma(self):
+        return self.a + self.b
+    def subtrai(self):
+        return  self.a - self.b
+    def multiplica(self):
+        return self.a * self.b
+    def divide(self):
+        return self.a / self.b
+
+c = Calculadora(a, b)
+
+print('Soma: ', c.soma())
+print('Subtração: ', c.subtrai())
+print('Multiplicação: ', c.multiplica())
+print('Divisão ', c.divide())
